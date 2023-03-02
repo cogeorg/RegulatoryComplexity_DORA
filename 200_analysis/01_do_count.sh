7637301898754a8f0608dc309360d577fc6f07c3
Co-Pierre Georg <cogeorg@protonmail.com>
2023-03-02 08:25:48 +0100

    First set of results continued

--- 200_analysis/01_do_count.sh
+++ 200_analysis/01_do_count.sh
@@ -5,24 +5,14 @@
 # ./05_prepare_glossary.py ../020_word_lists/RegulatoryOperators_Tax-raw.txt ../020_word_lists/RegulatoryOperators_Tax-clean.txt
 
 # ANALYZE TEXT
-for entry in DORA1 ;
-  do time ./10_do_analysis.py ../010_raw_documents/$entry.txt $entry.csv;
-done
-
-# for entry in OECD Pillar1_ProfitSplit-Sections OECD_ProfitSplit-Sections UN_ProfitSplit-Sections ;
+# for entry in DORA1 ;
 #   do time ./10_do_analysis.py ../010_raw_documents/$entry.txt $entry.csv;
 # done
-# for entry in OECD_DisputeSettlement-Sections Pillar1_DisputeSettlement-Sections;
-#   do time ./10_do_analysis.py ../010_raw_documents/$entry.txt $entry.csv;
-# done
-
-
-
 
 # rm results/results.csv 2>/dev/null
 # cat results/results*.csv >> results/results.csv
 
 # # CREATE HISTOGRAMS
-# for entry in Pillar1 Pillar2 OECD OECD_NoGlossary UN UN_NoGlossary UN_NoGlossary_NoCountry Pillar1_ProfitSplit-Sections OECD_ProfitSplit-Sections UN_ProfitSplit-Sections OECD_DisputeSettlement-Sections Pillar1_DisputeSettlement-Sections;
+# for entry in DORA1;
 #   do ./11_create_histograms.py ./frequency/frequency-$entry.csv ./histograms/histo-$entry.csv ;
 # done
