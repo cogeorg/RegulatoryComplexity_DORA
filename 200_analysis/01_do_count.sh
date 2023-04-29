42c300cbd75e550409364abd546156e4e51594b3
Co-Pierre Georg <cogeorg@protonmail.com>
2023-04-29 08:22:37 +0200

    Worked on histograms

--- 200_analysis/01_do_count.sh
+++ 200_analysis/01_do_count.sh
@@ -13,18 +13,18 @@
 #   do time ./10_do_analysis.py ../010_raw_documents/DORA_FinReg-2022--$entry.txt DORA_FinReg-2022--$entry.csv;
 # done
 
-for entry in  MICA MICA_IA AI AI_IA;
-  do time ./10_do_analysis.py ../010_raw_documents/$entry.txt $entry.csv;
-done
+# for entry in  MICA MICA_IA AI AI_IA;
+#   do time ./10_do_analysis.py ../010_raw_documents/$entry.txt $entry.csv;
+# done
 
 # rm results/results.csv 2>/dev/null
 # echo "file_name;LogicalConnectors;UniqueLogicalConnectors;Operands;UniqueOperands;Operators;UniqueOperators;Other;UniqueOther;Unclassified;UniqueUnclassified;WordCount" > results/results.csv
 # cat results/results*.csv |grep -v "file_name;Operands" >> results/results.csv
 
-rm results/latex_results.tex 2>/dev/null
-cat results/*.tex >> results/latex_results.tex
+# rm results/latex_results.tex 2>/dev/null
+# cat results/*.tex >> results/latex_results.tex
 
 # CREATE HISTOGRAMS
-# for entry in DORA_EP_PA-2022 DORA_FinReg-2022 DORA_Initial-2020 DORA_Initial-2020_IA MICA MICA_IA AI AI_IA;
-#   do ./11_create_histograms.py ./frequency/frequency-$entry.csv ./histograms/histo-$entry.csv ;
-# done
+for entry in DORA_EP_PA-2022 DORA_FinReg-2022 DORA_Initial-2020 DORA_Initial-2020_IA MICA MICA_IA AI AI_IA;
+  do ./11_create_histograms.py ./frequency/frequency-$entry.csv ./histograms/histo-$entry.csv ;
+done
